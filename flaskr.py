ce346436af55085ae30b32a2271ae95294a0552c
MrLokans <trikster1911@gmail.com>
2016-02-28 09:47:14 +0300

    Database initialisation method added.

--- flaskr.py
+++ flaskr.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from contextlib import closing
+
 from flask import (
     Flask, request, session, g, redirect,
     url_for, abort, render_template, flash
@@ -16,6 +18,12 @@ PASSWORD = 'default_password'
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
 
 app = Flask(__name__)
 # look for all the uppercase values in the specified module.
