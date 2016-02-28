19660e5737b998f86039109950297c83ceebb28b
MrLokans <trikster1911@gmail.com>
2016-02-28 10:37:07 +0300

    Login/logout tests added.

--- flaskr_tests.py
+++ flaskr_tests.py
@@ -17,9 +17,36 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(flaskr.app.config['DATABASE'])
 
+    def login(self, username, password):
+        return self.app.post('/login',
+                             data={"username": username, "password": password},
+                             follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
     def test_empty_db(self):
         rcvd = self.app.get('/')
         self.assertIn("No entries.", rcvd.data.decode('utf-8'))
+
+    def test_logs_in(self):
+        rcvd = self.login('admin', 'default_password')
+        self.assertIn('Successfully logged in.', rcvd.data.decode('utf-8'))
+
+    def test_logs_out(self):
+
+        rcvd = self.logout()
+        self.assertIn('Logged out', rcvd.data.decode('utf-8'))
+
+    def test_does_not_log_in_with_wrong_username(self):
+
+        rcvd = self.login('invaliduser', 'default_password')
+        self.assertIn('Invalid username', rcvd.data.decode('utf-8'))
+
+    def test_does_not_log_in_with_incorrect_password(self):
+
+        rcvd = self.login('admin', 'invalid_password')
+        self.assertIn('Invalid password', rcvd.data.decode('utf-8'))
 
 if __name__ == '__main__':
     unittest.main()
