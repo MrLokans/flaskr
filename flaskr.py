fe0323c9fd9287fa319bc5b449850c0d36a847ae
MrLokans <trikster1911@gmail.com>
2016-02-28 10:15:12 +0300

    Login/logout views added. Posting added.

--- flaskr.py
+++ flaskr.py
@@ -42,5 +42,46 @@ def teardon_requesT(exception):
     if db is not None:
         db.close()
 
+
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('SELECT title, text FROM entries ORDER BY id DESC')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    g.db.execute('INSERT INTO entries (title, text) VALUES (?, ?)',
+                 [request.form['title'], request.form['text']])
+    g.db.commit()
+    flash('New entry added.')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Successfully logged in.')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('Logged out')
+    return redirect(url_for('show_entries'))
+
+
 if __name__ == '__main__':
     app.run()
